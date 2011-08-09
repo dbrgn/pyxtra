a501b87620cf039ef263365424b4cfa4acc45db4
Peter Manser <mail@petermanser.ch>
2011-08-09 21:47:03 +0200

    Version update

--- pyxtra.py
+++ pyxtra.py
@@ -4,7 +4,7 @@
 """A small commandline utility written in Python to access the Swisscom
 Xtrazone SMS service
 
-Version: 1.2
+Version: 1.3
 
 License:
 Copyright (C) 2011 Danilo Bargen, Peter Manser
